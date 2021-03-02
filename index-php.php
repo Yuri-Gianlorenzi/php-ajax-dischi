5ea7f4fedbbcc5e2a18338bdc39bb0ec9b640460
Yuri Gianlorenzi <yurigian@gmail.com>
2021-03-02 15:56:26 +0100

    add php to print discs, need to fix style with scss

--- index-php.php
+++ index-php.php
@@ -16,12 +16,46 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
   <!-- link foglio di stile -->
-  <link rel="stylesheet" href="dist/app.css">
+  <link rel="stylesheet" href="dist/app.css?v=<?php echo time(); ?>">
   <title>Dischi PHP</title>
 </head>
 <body>
 
-  <?php
-  echo $discs[0]['title']; ?>
+
+  <header>
+      <div class="container">
+          <img src="img/logo.png" alt="logo">
+      </div>
+  </header>
+
+  <div class="cds-container container">
+      <!-- Disco ad esempio -->
+      <label for="genre">Scegli il genere</label>
+      <div class="options">
+        <select name="genre" id="genre">
+          <!-- <option value="">All</option> -->
+          <option>ciao</option>
+          <option>ciao</option>
+          <option>ciao</option>
+        </select>
+
+      </div>
+
+      <?php foreach ($discs as $disc) {?>
+
+      <div class="cd">
+        <img src="<?php echo $disc['poster']; ?>" alt="<?php echo $disc['title']; ?>">
+        <h3><?php echo $disc['title']; ?></h3>
+        <span class="author"><?php echo $disc['author']; ?></span>
+        <span class="year"><?php echo $disc['year']; ?></span>
+      </div>
+
+    <?php } ?>
+
+
+
+
+  </div>
+
 </body>
 </html>
