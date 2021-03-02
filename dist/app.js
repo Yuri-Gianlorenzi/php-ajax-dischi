232818a2d396311077f23e559a579ae7313bc698
Yuri Gianlorenzi <yurigian@gmail.com>
2021-03-02 17:14:24 +0100

    add milestone 2 with filter and same style

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,62 @@
   \********************/
 /***/ (() => {
 
+//effettuando controllo con postman abbiamo la certezza di aver creato correttamente un file json. è sytato creato un file server.php dove viene specificatodi codificare il contenuto di db.php in un file json. dunque la chiamata axios deve essere fatta al link del file server.php. stando attentii a come è formato, possiamo stamparne il contenuto in html
+var app = new Vue({
+  el: '#app',
+  data: {
+    films: '',
+    //per manipolare l'oggetto ricevuto dalla chiamata
+    genere: ['All'],
+    //per creare il filtro per genere
+    genreList: '',
+    //per creare la lista dei generi nella select
+    genreChoosed: '' //gli viene assegnato il genere selezionato per riuscire a filtrare
 
+  },
+  //fine data
+  mounted: function mounted() {
+    var _this = this;
+
+    //qui effettuiamo la chiamata al NOSTRO SERVER
+    axios.get('http://localhost/02_marzo/php-ajax-dischi/server.php').then(function (result) {
+      console.log(result.data); //con il console vediamo il percorso all'interno dell'api per arrivare ai dati che ci interessano, nel nostro caso, all'interno della key 'data' abbiamo i nostri oggetti(cd)
+      //uguagliamo all'array vuoto per poter manipolare al meglio il nostro array di oggetti
+
+      _this.films = result.data; //qui filtriamo l'oggetto della chiamata al nostro server per creare un array che contenga tutti i generi musicali presenti nell'oggetto ricevuto, questo array serve per il v-for della select
+
+      for (var i = 0; i < _this.films.length; i++) {
+        if (!_this.genere.includes(_this.films[i].genre)) {
+          _this.genere.push(_this.films[i].genre);
+        }
+      }
+    }); //fine then axios
+  },
+  //fine mounted
+  methods: {
+    //questa funzione ci permette di filtrare il nostro oggetto e dunque stampare solo i cd del genere musicale selezionato
+    chooseGenre: function chooseGenre(tipo) {
+      console.log(tipo); //uguagliamo la variabile al genere selezionato dall'utente
+
+      this.genreChoosed = tipo; //con il filter creiamo un'array che ha al suo interno contiene SOLO gli elementi che hanno come key-genere lo stesso valore selezionato dall'utente
+
+      var filtro = this.films.filter(function (element) {
+        return element.genre == tipo;
+      });
+      console.log(filtro); // se il filtro ha successo (ci sono elementi nel nostro array che hanno lo stesso genere di quello selezionato dall'utente nella select), uguagliamo questo filtro alla variabile vuota creata nei data. quando questa NON È VUOTA, L'HTML STAMPERÀ QUESTA e non la lista totale (tramite v-if e v-else). quando per esempio si riclicca su all nella select, genre list viene riportata vuota e dunque verrà stampato quello indicato nel mounted
+
+      if (filtro.length > 0) {
+        this.genreList = filtro;
+      } else {
+        this.genreList = '';
+      }
+
+      console.log('lista ' + this.genreList);
+    } //fine chooseGenre
+
+  } //fine methods
+
+}); //fine vue
 
 /***/ }),
 
